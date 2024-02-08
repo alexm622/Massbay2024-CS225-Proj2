dd7e237f68af33442b27941de67c2e12baa6b86c
Alex <17982529+alexm622@users.noreply.github.com>
2024-02-08 11:34:13 -0500

    Hello World

---
+++ com/example/Main.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+//generate a javafx stub
+
+public class Main extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        primaryStage.show();
+    }
+}
