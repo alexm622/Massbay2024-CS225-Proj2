36404666bd58662988ca94fb29f827050a3e2926
Alex <17982529+alexm622@users.noreply.github.com>
2024-02-22 11:22:18 -0500

    make the generated puzzle playable

--- com/example/Puzzle.java
+++ com/example/Puzzle.java
@@ -5,9 +5,10 @@ import java.util.Random;
 
 public class Puzzle {
 
-    public Puzzle() {
-        generateRandomBoard();
-    }
+    public static final int WIDTH = 3;
+    public static final int HEIGHT = 3;
+    public static final int BOX_SIZE = 3;
+    public final int NUM_BOXES;
 
     /**
      * the first index is the row
@@ -15,19 +16,31 @@ public class Puzzle {
      * the third index is the index of the correct answer in that specific row in that specific box
      */
     private int[][][] solutionBoard; 
-    private int[][][] currentBoard = new int[3][3][3];
+    private int[][][] currentBoard = new int[WIDTH][HEIGHT][BOX_SIZE];
+
+    public Puzzle() {
+        int numBoxes = 0;
+        for (int i = WIDTH; i > 0; i--) {
+            numBoxes += i;
+        }
+        NUM_BOXES = numBoxes;
+        generateRandomBoard();
+    }
+
+
+    
 
     /**
      * generates a random board 
      */
     public void generateRandomBoard() {
         // generate a random board
-        solutionBoard = new int[3][3][3]; // 3 rows, 3 boxes max width, 3 answers per box
+        solutionBoard = new int[WIDTH][HEIGHT][BOX_SIZE]; // 3 rows, 3 boxes max width, 3 answers per box
 
         // fill with -1
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                for (int k = 0; k < 3; k++) {
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                for (int k = 0; k < BOX_SIZE; k++) {
                     solutionBoard[i][j][k] = -1;
                 }
             }
@@ -38,13 +51,39 @@ public class Puzzle {
         Random r = new Random();
 
         //build the board
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3 - width; j++) {
-                for (int k = 0; k < 3; k++) {
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT - width; j++) {
+                for (int k = 0; k < BOX_SIZE; k++) {
                     solutionBoard[i][j][k] = r.nextInt(3);
                 }
             }
             width++;
+        }
+        //run corrections
+        runCorrections(solutionBoard);
+    }
+
+    /***
+     * runs corrections on the board
+     * @param board the board to correct
+     */
+    private void runCorrections(int[][][] board){
+        //run corrections on the board
+        //check for any rows that have the same answer in the same box
+        //if there are any, change one of the answers to a different answer
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                for (int k = 0; k < BOX_SIZE; k++) {
+                    int answer = board[i][j][k];
+                    for (int l = 0; l < BOX_SIZE; l++) {
+                        if (l != k && board[i][j][l] == answer) {
+                            //change the answer
+                            int newAnswer = (answer + 1) % 3;
+                            board[i][j][l] = newAnswer;
+                        }
+                    }
+                }
+            }
         }
     }
 
@@ -58,14 +97,14 @@ public class Puzzle {
         // being the index of the 1
 
         String[] buffer = new String[3];
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < WIDTH; i++) {
             buffer[i] = "";
         }
 
         int width = 0;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3  - width; j++) {
-                for (int k = 0; k < 3; k++) {
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT  - width; j++) {
+                for (int k = 0; k < BOX_SIZE; k++) {
                     //get the answer
                     int answer = board[i][j][k];
                     //print the row
@@ -80,7 +119,7 @@ public class Puzzle {
                 
             }
             //print the buffer
-            for (int l = 0; l < 3; l++) {
+            for (int l = 0; l < WIDTH; l++) {
                 System.out.println(buffer[l]);
                 buffer[l] = "";
             }
@@ -103,9 +142,9 @@ public class Puzzle {
             return false;
         }
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                for (int k = 0; k < 3; k++) {
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                for (int k = 0; k < BOX_SIZE; k++) {
                     if (board[i][j][k] != solutionBoard[i][j][k]) {
                         return false;
                     }
